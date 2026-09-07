eab64b448652f8962eee51bffa18f0ac8c1ba47f
AaryaWargaonkar <aaryawargaonkar@gmail.com>
2026-09-07 15:31:43 +0530

    Update ManageQuiz.jsx

--- frontend/src/components/ManageQuiz.jsx
+++ frontend/src/components/ManageQuiz.jsx
@@ -26,7 +26,7 @@ const ManageQuiz = () => {
             correctAnswer
         }
 
-        axios.post("http://localhost:4000/quiz/add", data)
+        axios.post(`${import.meta.env.VITE_API_URL}/quiz/add`, data)
             .then(() => {
                 alert("Question added successfully")
                 setQuestion("")
