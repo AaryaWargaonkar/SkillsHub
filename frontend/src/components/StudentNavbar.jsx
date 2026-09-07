1381830dcf20287b2ad33c72b8625353d0289398
AaryaWargaonkar <aaryawargaonkar@gmail.com>
2026-09-07 16:53:25 +0530

    Update StudentNavbar.jsx

--- frontend/src/components/StudentNavbar.jsx
+++ frontend/src/components/StudentNavbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
 const StudentNavbar = () => {
   const navigate = useNavigate();
